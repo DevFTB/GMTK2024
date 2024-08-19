8e1def3ef15edfe19b6252f5720ec1c5c1866920
Aditha Doratiyawa <indegamedev@gmail.com>
2024-08-20 01:08:30 +1000

    small player can't depress pressure plates

--- level/components/interactor/player_interactor.gd
+++ level/components/interactor/player_interactor.gd
@@ -1,6 +1,8 @@
 extends Area2D
 class_name PlayerInteractor
 
+@export_flags("Small", "Normal", "Big") var size_flags : int = 7
+
 signal player_entered(player: Player)
 signal player_exited(player: Player)
 
@@ -11,6 +13,8 @@ var _disabled := false
 var has_player: bool:
 	get:
 		return interacting_player != null
+		
+var right_player_size: bool = false
 
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
@@ -18,13 +22,18 @@ func _ready() -> void:
 	
 func _on_body_entered(body: Node2D) -> void:
 	if body is Player and not _disabled:
-		interacting_player = body
-		player_entered.emit(body)
+		right_player_size = size_flags & 2 ** body.size_mode
+		if right_player_size:
+			interacting_player = body
+			player_entered.emit(body)
 	
-func _on_body_exited(body: Node2D) -> void:
+func _on_body_exited(body: Node2D) -> void:		
 	if body is Player and not _disabled:
 		interacting_player = null
-		player_exited.emit(body)
+		
+		if right_player_size:
+			player_exited.emit(body)
+
 
 func set_disabled(value: bool) -> void:
 	_disabled = value
