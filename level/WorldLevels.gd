5a1f8f848e2da6852a7ca9d93852f145bcbc3754
Arthur <thepandaparadox@gmail.com>
2024-08-19 12:13:56 +1000

    Added music and music player
    ensure all worlds have 'music_player.tscn'  as a child

--- level/WorldLevels.gd
+++ level/WorldLevels.gd
@@ -2,7 +2,12 @@ extends Resource
 class_name WorldLevels
 
 @export var levels: Dictionary
+@export var level_music: Dictionary
 
 func get_level(level_name: String) -> PackedScene:
 	assert(levels.has(level_name), "level %s not in levels dictionary" % level_name)
 	return levels[level_name]
+	
+func get_music(level_name: String) -> AudioStream:
+	assert(level_music.has(level_name), "music %s not in musicdictionary" % level_name)
+	return level_music[level_name]
