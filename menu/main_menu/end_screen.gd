5c5b1bf8c7d36bc7dcd3ee72b264879bc49468fe
Aditha Doratiyawa <indegamedev@gmail.com>
2024-08-21 03:08:29 +1000

    Itch.IO Release

--- menu/main_menu/end_screen.gd
+++ menu/main_menu/end_screen.gd
@@ -2,9 +2,9 @@ extends Control
 
 @export var main_menu_scene: PackedScene
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
-
+const MAIN_MENU = preload("res://menu/main_menu/main_menu.tscn")
 func _ready() -> void:
 	animation_player.play("show")
 
 func _on_confirm_button_pressed() -> void:
-	get_tree().change_scene_to_packed(main_menu_scene)
+	get_tree().change_scene_to_packed(MAIN_MENU)
