53db1434eaa05e4412f74586f215e352952258ae
Ryan <ryanwp526@gmail.com>
2024-08-21 01:49:15 +1000

    a

--- main.gd
+++ main.gd
@@ -1,16 +1,7 @@
 extends Node
 
 @onready var player: Player = $World/Player
-@onready var minimap: Control = %Minimap
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("debug3"):
 		player.kill()
-	
-	if event.is_action_pressed("minimap"):
-		if not minimap.visible:
-			minimap.open()
-			player.paused = true
-		else:
-			minimap.hide()
-			player.paused = false
